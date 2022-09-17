2e0e89abc38dbdea1b762ed898e46756c63ec2f7
Iliraa <61836150+Ilirr@users.noreply.github.com>
2022-09-18 00:11:03 +0200

    final

---
+++ Websocket/Server.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace Websocket
+{
+    public class Server
+    {
+      public void Test()
+      {
+         ExecuteServer();
+      }
+      static void ExecuteServer()
+      {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 23501);
+
+
+            Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+
+
+                listener.Bind(localEndPoint);
+
+
+                listener.Listen(10);
+
+                while (true)
+                {
+
+                    Console.WriteLine("Waiting connection ... ");
+
+                    Socket clientSocket = listener.Accept();
+
+                    byte[] bytes = new Byte[1024];
+                    string data = null;
+
+                    while (true)
+                    {
+
+                        int numByte = clientSocket.Receive(bytes);
+
+                        data += Encoding.ASCII.GetString(bytes,0, numByte);
+
+                        if (data.IndexOf("<EOF>") > -1)
+                            break;
+                    }
+
+                    while(true)
+                    {
+                        Console.WriteLine("Text received -> {0} ", data);
+                        byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+                        clientSocket.Send(message);
+                        
+                    }
+
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+    }
+}
